06490405a8e66175dd1eab7078b982ca53bfff87
tonyNguyen64 <tonynguyen@Tonys-MacBook-Pro.local>
2025-03-07 15:13:31 -0500

    configure security configuratio files

--- managementNailClinic/src/main/java/com/nailclinic/managementnailclinic/Configurations/Services/Configures/SecurityConfiguration.java
+++ managementNailClinic/src/main/java/com/nailclinic/managementnailclinic/Configurations/Services/Configures/SecurityConfiguration.java
@@ -57,7 +57,7 @@ public class SecurityConfiguration {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("https://vipnails.me"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type", "X-Requested-With"));
         configuration.setAllowCredentials(true);
